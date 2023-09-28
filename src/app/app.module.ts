92955c131450778661f69ca7e5040fcc93ecaec5
Diyouf <diyoufkv7@gmail.com>
2023-09-28 20:41:00 +0530

    game Logic Almost Comleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameTempleteComponent } from './component/game-templete/game-templete.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { GameTempleteComponent } from './component/game-templete/game-templete.c
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/game-templete/game-templete.component.ts
+++ src/app/component/game-templete/game-templete.component.ts
@@ -1,14 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-game-templete',
   templateUrl: './game-templete.component.html',
   styleUrls: ['./game-templete.component.css']
 })
-export class GameTempleteComponent {
-  constructor(){}
+export class GameTempleteComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void {
+    this.startGame()
+  }
 
-  iconsArray: string[] = ['🎶','☠️','🤖','🐚','🕊️','🦚']
-  DuplicateArray:string[] = [...this.iconsArray,...this.iconsArray]
+  iconsArray: string[] = ['🎶', '☠️', '🤖', '🐚', '🕊️', '🦚']
+  DuplicateArray: string[] = [...this.iconsArray, ...this.iconsArray]
+  shuffeledArray: { icon: string, flipped: boolean, solved: boolean, position: number }[] = []
+  timeout!: any
+
+  startGame() {
+    while (this.shuffeledArray.length < this.iconsArray.length * 2) {
+      let randomIndex1 = Math.floor((Math.random() * this.DuplicateArray.length));
+      this.shuffeledArray.push({
+        icon: this.DuplicateArray[randomIndex1],
+        flipped: false,
+        solved: false,
+        position: this.shuffeledArray.length
+      })
+      this.DuplicateArray.splice(randomIndex1, 1);
+    }
+  }
+
+  handleActive(data: { icon: string, flipped: boolean, solved: boolean, position: number }) {
+    const flippeddata = this.shuffeledArray.filter((item) => item.flipped && !item.solved)
+    if (flippeddata.length === 2) return
+    this.shuffeledArray.map((item) => {
+      if (item.position === data.position) {
+        item.flipped = !item.flipped
+      }
+    })
+
+    this.gameLogicForFlipped()
+  }
+
+
+  gameLogicForFlipped() {
+    const flippedData = this.shuffeledArray.filter(piece => piece.flipped && !piece.solved);
+    if (flippedData.length === 2) {
+      this.timeout = setTimeout(() => {
+        this.shuffeledArray = this.shuffeledArray.map(piece => {
+          if (
+            piece.position === flippedData[0].position ||
+            piece.position === flippedData[1].position
+          ) {
+            if (flippedData[0].icon === flippedData[1].icon) {
+              piece.solved = true;
+            } else {
+              piece.flipped = false;
+            }
+          }
+          return piece;
+        });
+      }, 800);
+    }
+  }
+
+  ngOnDestroy(): void {
+    clearTimeout(this.timeout);
+  }
+
+  isGameCompleted(): boolean {
+    return this.shuffeledArray.length > 0 && this.shuffeledArray.every(piece => piece.solved);
+  }
+
 
 }
